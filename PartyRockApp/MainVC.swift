9247d5fd96e2f56481e42c716fb6312342de653b
studentas <studentas@vduif-503-04.local>
2018-01-02 12:35:50 +0200

    imageView and btns added

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by studentas on 02/01/2018.
+//  Copyright © 2018 studentas. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
